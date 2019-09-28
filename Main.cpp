aedbbb7b280d661b5888346f39258fc3f1dfd69f
Mihiru <mihirukongahage@gmail.com>
2019-09-28 18:46:28 +0530

    finalizing

--- Main.cpp
+++ Main.cpp
@@ -17,25 +17,29 @@ int main()
         string username;
         string password;
 
+        // User Authenticating
         cout << "Enter username : ";
         cin >> username;
         cout << "Enter password : ";
         cin >> password;
         if (username == "nipuna" && password == "nipuna")
         {
-            cout << "logged in";
+            cout << "logged in\nUser " << username << endl;
             while (1)
             {
                 //Instruction window
                 cout << "\n//////////Instructions//////////\n\n";
                 cout << "1 -> Create a temperature log\n";
                 cout << "2 -> Read the temperature of the last 4 weeks\n";
-                cout << "3 -> Compare the temperature variance in the last 4 weeks\n\n";
+                cout << "3 -> Compare the temperature variance in the last 4 weeks\n";
+                cout << "4 -> Exit\n\n";
 
+                //Get the user instruction number as 'inst'
                 int inst;
                 cout << "Enter instruction number : ";
                 cin >> inst;
 
+                // If the user instruction is 1-> Create a new temperature log
                 if (inst == 1)
                 {
                     // Get motor number
@@ -44,35 +48,40 @@ int main()
                     cin >> motorNumber;
 
                     // Availability of the file
-                    ifstream ifile(motorNumber + ".txt");
+                    ifstream ifile("Motors/" + motorNumber + ".txt");
+                    // If the motor is available in the database
                     if (ifile)
                     {
-                        cout << "File present";
+                        cout << "Motor details available\n";
 
+                        //Get shaft temperature as 'shaftTemp'
                         int shaftTemp;
                         cout << "Enter motor shaft temperature : ";
                         cin >> shaftTemp;
 
+                        //Get shaft temperature as 'centerTemp'
                         int centerTemp;
                         cout << "Enter motor center temperature : ";
                         cin >> centerTemp;
 
+                        //Get shaft temperature as 'fanTemp'
                         int fanTemp;
                         cout << "Enter fan temperature : ";
                         cin >> fanTemp;
 
                         // Create a Motor class
                         Motor M;
-                        M.number = motorNumber;
-                        M.shaftTemp = shaftTemp;
-                        M.centerTemp = centerTemp;
-                        M.fanTemp = fanTemp;
+                        M.number = motorNumber;    // Unique number to identify the motor
+                        M.shaftTemp = shaftTemp;   // Shaft temperature
+                        M.centerTemp = centerTemp; // Center temperature
+                        M.fanTemp = fanTemp;       // Fan temperature
 
+                        // Class method to log temperature details
                         M.create_temp_log();
                     }
                     else
                     {
-                        cout << "No file";
+                        cout << "No motor details available\n";
 
                         string motorName;
                         cout << "Enter motor name : ";
@@ -98,6 +107,7 @@ int main()
                         M.centerTemp = centerTemp;
                         M.fanTemp = fanTemp;
 
+                        // Create a new temperature log
                         M.new_create_temp_log();
                     }
                 }
@@ -111,11 +121,24 @@ int main()
                     Motor M;
                     M.number = motorNumber;
 
+                    // Read temperature method
                     M.read_temperature();
                 }
                 else if (inst == 3)
                 {
-                    //
+                    // Get motor number
+                    string motorNumber;
+                    cout << "Enter motor number : ";
+                    cin >> motorNumber;
+
+                    Motor M;
+                    M.number = motorNumber;
+                    // Analyze temperature
+                    M.analyze_temperature();
+                }
+                else if (inst == 4)
+                {
+                    exit(1);
                 }
                 else
                 {
--- MotorClass.h
+++ MotorClass.h
@@ -5,29 +5,48 @@
 #include <ctime>
 #include <vector>
 #include <bits/stdc++.h>
+#include <sstream>
 
 using namespace std;
 
+// Function to split a string
+// Return a vector of type string
+// This function tokenize a string with a given delimiter
+vector<string> split(const string &s, char delimiter)
+{
+    vector<string> tokens;
+    string token;
+    istringstream tokenStream(s);
+    while (getline(tokenStream, token, delimiter))
+    {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+// Motor class
 class Motor
 {
 public:
     // Motor details
-    char *dateTime;
-    string number;
-    string name;
+    char *dateTime; // Date and time
+    string number;  // Motor number
+    string name;    // Motor name
 
     // Temperature details of the mortor
     int shaftTemp;
     int centerTemp;
     int fanTemp;
 
-    //Create a temperature log
+    //Create a new temperature log
     void new_create_temp_log()
     {
+        // Get the current date and time
         time_t now = time(0);
         char *dt = ctime(&now);
         dateTime = dt;
 
+        // Write a log to the file
         ofstream file;
         file.open("Motors/" + number + ".txt");
         file << number << endl
@@ -40,19 +59,22 @@ public:
              << ","
              << "Date and Time"
              << endl;
-        file << shaftTemp << "," << centerTemp << "," << fanTemp << "," << dateTime;
+        file << shaftTemp << " " << centerTemp << " " << fanTemp << " " << dateTime;
         file.close();
     }
 
+    // Create a temperature log to an existing file
     void create_temp_log()
     {
+        // Motor details
         time_t now = time(0);
         char *dt = ctime(&now);
         dateTime = dt;
 
+        // Write a log to the file
         ofstream file;
         file.open("Motors/" + number + ".txt", ios::app);
-        file << shaftTemp << "," << centerTemp << "," << fanTemp << "," << dateTime;
+        file << shaftTemp << " " << centerTemp << " " << fanTemp << " " << dateTime;
         file.close();
     }
 
@@ -61,11 +83,62 @@ public:
     {
 
         int i = 0;
+        // Initialize a vector of type string
         vector<string> array;
         string STRING;
         ifstream file;
         file.open("Motors/" + number + ".txt");
-        cout << "__________________________________________";
+        while (getline(file, STRING))
+        {
+            if (i == 0)
+            {
+                // Print the motor number
+                cout << "Motor Number : ";
+                cout << STRING << endl;
+            }
+            else if (i == 1)
+            {
+                // Print the motor name
+                cout << "Name of the motor : ";
+                cout << STRING << endl;
+            }
+            else if (i == 2)
+            {
+                // Print the header string
+                cout << STRING
+                     << endl;
+            }
+            else
+            {
+                // Push the file row to the vector
+                array.push_back(STRING);
+            }
+            i++;
+        }
+
+        // Print the last 4 records
+        int j = 0;
+        // Reverse the vector to get the latest data first
+        reverse(array.begin(), array.end());
+        for (int i = 0; i < array.size(); i++)
+            if (i == 4)
+            {
+                break;
+            }
+            else
+            {
+                cout << array[i] << "\n";
+            }
+    }
+
+    //Compare the temperature variance in the last 4 weeks
+    void analyze_temperature()
+    {
+        int i = 0;
+        vector<string> array;
+        string STRING;
+        ifstream file;
+        file.open("Motors/" + number + ".txt");
         while (getline(file, STRING))
         {
             if (i == 0)
@@ -93,6 +166,10 @@ public:
 
         // Print the last 4 records
         int j = 0;
+        int arr_shaft[4];
+        int arr_center[4];
+        int arr_fan[4];
+
         reverse(array.begin(), array.end());
         for (int i = 0; i < array.size(); i++)
             if (i == 4)
@@ -101,15 +178,72 @@ public:
             }
             else
             {
-                cout << array[i] << "\n";
+                string values[3];
+                // Split the row and insert it into vector v
+                vector<string> v = split(array[i], ' ');
+                int j = 0;
+                for (auto i : v)
+                {
+                    if (j > 2)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        // Push the values to a new array 'values'
+                        values[j] = i;
+                    }
+                    j++;
+                }
+
+                // Convert the string vector to int vector
+                vector<int> int_v;
+                for (int k = 0; k < 4; k++)
+                {
+                    int num = atoi(v.at(k).c_str());
+                    int_v.push_back(num);
+                }
+
+                // 3 new arrays to store details
+                arr_shaft[i] = int_v[0];
+                arr_center[i] = int_v[1];
+                arr_fan[i] = int_v[2];
             }
 
-        cout << "__________________________________________";
-    }
+        // For shaft
+        cout << "Shaft Temperature Data\n";
+        for (int m = 0; m < 4; m++)
+        {
+            cout << "Week " << m + 1 << " : " << arr_shaft[4 - m - 1] << endl;
+        }
+        if (arr_shaft[0] >= arr_shaft[1])
+        {
+            cout << "Warning!!! Shaft Temperature Increased\n";
+        }
+        cout << '\n';
 
-    //Compare the temperature variance in the last 4 weeks
-    void analyze_temperature()
-    {
-        //
+        // For center
+        cout << "Center Temperature Data\n";
+        for (int m = 0; m < 4; m++)
+        {
+            cout << "Week " << m + 1 << " : " << arr_center[4 - m - 1] << endl;
+        }
+        if (arr_center[0] >= arr_center[1])
+        {
+            cout << "Warning!!! Center Temperature Increased\n";
+        }
+        cout << '\n';
+
+        // For fan
+        cout << "Fan Temperature Data\n";
+        for (int m = 0; m < 4; m++)
+        {
+            cout << "Week " << m + 1 << " : " << arr_fan[4 - m - 1] << endl;
+        }
+        if (arr_fan[0] >= arr_fan[1])
+        {
+            cout << "Warning!!! Fan Temperature Increased\n";
+        }
+        cout << '\n';
     }
 };
